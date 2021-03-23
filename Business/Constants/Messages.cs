8d9d51f4e0afecfdb5851481fa12a2d3e71ffe22
SkymanSky <gokera@ogr.iu.edu.tr>
2021-03-23 21:50:37 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "Prodcut name invalid";
+        internal static string MaintenanceTime="Maintenance Time";
+        internal static string ProductsListed="Products Listed";
+    }
+}
