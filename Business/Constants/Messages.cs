f362d69abd1bb4058edc204a5ebec826dda3e706
SkymanSky <gokera@ogr.iu.edu.tr>
2021-04-02 00:47:36 +0300

    14.Ders

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using Entites.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +15,6 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists="Bu isimde bir ürün var.";
         public static string CategoryLimitExceded="Kategori limiti aşıldı.";
+        public static string AuthorizationDenied="Erişim Engellendi";
     }
 }
